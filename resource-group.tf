3cc455ed3ae36a555d67e3f5be0c0618afa52829
Bheesetty, Bindu Madhavi <157078264+BinduBheesetty@users.noreply.github.com>
2024-04-20 17:34:33 -0400

    updated

--- resource-group.tf
+++ resource-group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "practicum16"{
-    name = "practicum16"
-    location = "eastus"
+resource "azurerm_resource_group" "practicum16bibhee"{
+    name = var.app_name
+    location = var.location
 }
